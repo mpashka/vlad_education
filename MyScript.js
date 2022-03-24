eef0ed83e1ce667a7363218b43a7e4de2f0c68ce
GveeN <libertyhimki@gmail.com>
2022-03-24 12:05:35 +0300

    first version

--- MyScript.js
+++ MyScript.js
@@ -1,44 +1,103 @@
-const WINNING_COMBINATION = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 7]
+  [0, 4, 8],
+  [2, 4, 6]
 ]
-let board = [
-  [0,0,0],
-  [0,0,0],
-  [0,0,0],
-];
 
-/*blocks.onclick = function() { alert(1); }*/
-const blocks = document.querySelectorAll('[data-cell]')
-blocks.forEach(block =>{
-  block.addEventListener('click', operateClick, { once: true })
-})
-function operateClick(event){
-  console.log('clicked')
+let cellElements
+let circleTurn
+let restartButton
+
+window.addEventListener("load", function(event) {
+  __init()
+});
+
+
+function __init(){
+  cellElements = document.querySelectorAll('[data-cell]')
+  restartButton = document.getElementById('restartButton')
+
+  restartButton.addEventListener('click', startGame)
+
+  startGame()
 }
 
-game()
+function startGame() {
+  circleTurn = false
 
-class Block {
-  constructor(id) {
-    this.id = id;
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.addEventListener('click', registerClick, { once: true })
+  })
+}
+
+function registerClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+
+  if (checkWin(CIRCLE_CLASS) || checkWin(X_CLASS)) return
+
+  placeMark(cell, currentClass)
+
+  if (checkWin(currentClass)) {
+    return endGame(1) // Winner is Defined
   }
-
-  addSymbol_X(block) {
+  else if (checkDraw()){
+    return endGame(2) // It's Draw
   }
+  else
+    swapTurns()
+}
 
-  addSymbol_O(block) {
+function endGame(stateOfGame){
+  let message
+
+  switch (stateOfGame){
+    case 1:
+      console.log(message = circleTurn ? "Circle player is winner" : "X player is winner")
+      break
+    case 2:
+      console.log(message = "Nobody wins. It's Draw!")
+      break
   }
 }
 
-
-function game(){
-  let X_turn = true;
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
 }
 
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function checkWin(currentClass){
+  /*if (cellElements[0][0].classList.contains(currentClass) && cellElements[0][1].classList.contains(currentClass) && cellElements[0][2].classList.contains(currentClass))
+    console.log(true)
+  if (cellElements[1][0].classList.contains(currentClass) && cellElements[1][1].classList.contains(currentClass) && cellElements[1][2].classList.contains(currentClass))
+    console.log(true)
+  if (cellElements[2][0].classList.contains(currentClass) && cellElements[2][1].classList.contains(currentClass) && cellElements[2][2].classList.contains(currentClass))
+    console.log(true)
+  if (cellElements[3][0].classList.contains(currentClass) && cellElements[3][1].classList.contains(currentClass) && cellElements[3][2].classList.contains(currentClass))
+    console.log(true)
+  if (cellElements[4][0].classList.contains(currentClass) && cellElements[4][1].classList.contains(currentClass) && cellElements[4][2].classList.contains(currentClass))
+    console.log(true)
+  if (cellElements[5][0].classList.contains(currentClass) && cellElements[5][1].classList.contains(currentClass) && cellElements[5][2].classList.contains(currentClass))
+    console.log(true)
+  if (cellElements[6][0].classList.contains(currentClass) && cellElements[6][1].classList.contains(currentClass) && cellElements[6][2].classList.contains(currentClass))
+    console.log(true)
+  if (cellElements[7][0].classList.contains(currentClass) && cellElements[7][1].classList.contains(currentClass) && cellElements[7][2].classList.contains(currentClass))
+    console.log(true)*/
+
+  return WINNING_COMBINATIONS.some(combination => combination.every(index => cellElements[index].classList.contains(currentClass)))
+}
+function checkDraw(){
+  return Array.from(cellElements).every(cell => cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS))
+}
